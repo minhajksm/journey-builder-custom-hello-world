765da912ff6cc3d54399c63980c6e6c563ff2a4f
minhajksm <minhaj.ksm@gmail.com>
2015-11-27 11:34:39 +0530

    Update config.js

--- public/ixn/activities/hello-world/config.js
+++ public/ixn/activities/hello-world/config.js
@@ -2,7 +2,7 @@ define([], function(){
     return {
         "icon": "images/jb-icon.jpg",
         "iconSmall": "images/jb-icon.jpg", 
-        "key": "jb-hello-world-activity-minhaj",
+        "key": "jb-hello-world-activity-minhaj2",
         "partnerApiObjectTypeId": "IXN.CustomActivity.REST",
         "lang": {
             "en-US": {        
